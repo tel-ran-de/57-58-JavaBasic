2c885de80becd787e300416b440e4bcb0a4142ae
StanislavPB <perminovsb@gmail.com>
2023-01-17 21:08:56 +0200

    add: lesson 5 lessoncode

--- src/lessons/lesson5/Robot.java
+++ src/lessons/lesson5/Robot.java
@@ -9,11 +9,18 @@ public class Robot {
     public Robot() {
     }
 
+    public Robot(String robotName, int model, boolean isPainter) {
+        this.robotName = robotName;
+        this.model = model;
+        this.isPainter = isPainter;
+    }
+
     public void hello(){
         System.out.println("Hello!");
     }
 
     public void greetings(){
+        System.out.println("--------------");
         System.out.println("Hello! My name is " + robotName);
         System.out.println("My model number is " + model);
         System.out.println("I am " + isPainter + " good painter");
--- src/lessons/lesson5/RobotDemo.java
+++ src/lessons/lesson5/RobotDemo.java
@@ -4,17 +4,32 @@ public class RobotDemo {
 
 
     public static void main(String[] args) {
+        //  создаем экземпляр класса РОБОТс помощью дефолтного конструктора
         Robot robotPetya = new Robot();
 
+        // печаем значение которое хранится в нашей переменной  -ссылка
         System.out.println(robotPetya);
 
+        // записываем значение в поля экземпляра класса ПЕТЯ
         robotPetya.robotName = "Petya";
         robotPetya.model = 25;
         robotPetya.isPainter = true;
 
-
+// обращаемся к методам  экземпляра класса ПЕТЯ
         robotPetya.hello();
         robotPetya.greetings();
+
+        //  создаем экземпляр класса РОБОТ с помощью нашего конструктора
+        Robot robotSiri = new Robot("Siri",5,false);
+
+        //// обращаемся к методу  экземпляра класса SIRI
+        robotSiri.greetings();
+
+        // Менякм значение в переменной экземпляра класса
+        robotSiri.model = 7;
+
+        // обращаемся к методу  экземпляра класса SIRI
+        robotSiri.greetings();
 
     }
 }
