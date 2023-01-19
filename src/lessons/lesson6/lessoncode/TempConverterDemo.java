b89bcecdc5e7d344d53d8db310ed72c65db9bbfb
StanislavPB <perminovsb@gmail.com>
2023-01-19 20:33:15 +0200

    add: lesson 6 lessoncode

---
+++ src/lessons/lesson6/lessoncode/TempConverterDemo.java
@@ -0,0 +1,16 @@
+package lessons.lesson6.lessoncode;
+
+public class TempConverterDemo {
+
+    public static void main(String[] args) {
+        Converter converter = new Converter();
+
+        Double tempL = ( (int  ) (100*converter.converterCelsiumToKelvin(21)))/100.0;
+
+        converter.printConverter("Convert temp from ", 21," Celsium in Kelvin is ",tempL);
+
+        converter.printConverter("Convert temp from ", 21," Celsium in Farengate is ", converter.converterCelsiumToFarengate(21));
+
+
+    }
+}
