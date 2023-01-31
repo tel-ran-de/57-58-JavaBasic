9edc45e3bab6b779493150691d59d3afb0fc7872
StanislavPB <perminovsb@gmail.com>
2023-01-31 22:55:04 +0200

    add: lesson 9 lessoncode

---
+++ src/lessons/lesson9/hardLevelEntity/DataTopLevel.java
@@ -0,0 +1,15 @@
+package lessons.lesson9.hardLevelEntity;
+
+public class DataTopLevel {
+    int idTop;
+
+    DataHighLevel dataHighLevel;
+
+    @Override
+    public String toString() {
+        return "DataTopLevel{" +
+                "idTop=" + idTop +
+                ", dataHighLevel=" + dataHighLevel +
+                '}';
+    }
+}
