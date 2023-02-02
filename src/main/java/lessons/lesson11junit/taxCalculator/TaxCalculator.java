1166b9fdab73035b6f6ad4e655db48548685aac0
StanislavPB <perminovsb@gmail.com>
2023-02-03 01:25:31 +0200

    add: lesson 11 Maven project transfer

--- src/main/java/lessons/lesson11junit/taxCalculator/TaxCalculator.java
+++ src/main/java/lessons/lesson11junit/taxCalculator/TaxCalculator.java
@@ -1,10 +1,10 @@
 package lessons.lesson11junit.taxCalculator;
 
-import teacher.annotations.CodeReview;
+
 
 import java.math.BigDecimal;
 
-@CodeReview(approved = true)
+
 public interface TaxCalculator {
     BigDecimal calculateTax(BigDecimal income);
 
