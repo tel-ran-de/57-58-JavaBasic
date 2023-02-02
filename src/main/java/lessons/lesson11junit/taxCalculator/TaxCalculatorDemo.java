28a5ba20cf94010a2d6e1a254987365a4c9e8825
StanislavPB <perminovsb@gmail.com>
2023-02-03 01:24:13 +0200

    add: lesson 11 Maven project transfer

---
+++ src/main/java/lessons/lesson11junit/taxCalculator/TaxCalculatorDemo.java
@@ -0,0 +1,23 @@
+package lessons.lesson11junit.taxCalculator;
+
+
+import java.math.BigDecimal;
+
+
+public class TaxCalculatorDemo {
+
+    public static void main(String[] args) {
+
+        TaxCalculatorImpl newCalculator = new TaxCalculatorImpl();
+
+        System.out.println(newCalculator.calculateTax(BigDecimal.valueOf(0.00)));
+
+        System.out.println(newCalculator.calculateTax(BigDecimal.valueOf(10000.00)));
+
+        System.out.println(newCalculator.calculateTax(BigDecimal.valueOf(20000.00)));
+
+        System.out.println(newCalculator.calculateTax(BigDecimal.valueOf(30000.00)));
+
+
+    }
+}
---
+++ src/main/java/lessons/lesson11junit/taxCalculator/TaxCalculatorImpl.java
@@ -0,0 +1,39 @@
+package lessons.lesson11junit.taxCalculator;
+
+
+import java.math.BigDecimal;
+
+public class TaxCalculatorImpl implements TaxCalculator {
+
+    @Override
+    public BigDecimal calculateTax(BigDecimal income) {
+
+
+        BigDecimal tax = BigDecimal.valueOf(0.00);
+        BigDecimal range_20000 = BigDecimal.valueOf(20000.00);
+        BigDecimal taxForRange_20000 = BigDecimal.valueOf(5000.00);
+
+
+        int resultOfCompare = income.compareTo(range_20000);
+
+
+        if (resultOfCompare==1) {
+            tax = sumBD(taxForRange_20000, multBD(BigDecimal.valueOf(0.40),subBD(income,range_20000)));
+
+        } else {tax =  income.multiply(BigDecimal.valueOf(0.25));}
+
+        return tax.setScale(2);
+    };
+
+    private BigDecimal sumBD(BigDecimal a, BigDecimal b){
+        return a.add(b);
+    }
+
+    private BigDecimal subBD(BigDecimal a, BigDecimal b){
+        return a.subtract(b);
+    }
+
+    private BigDecimal multBD(BigDecimal a, BigDecimal b){
+        return a.multiply(b);
+    }
+}
