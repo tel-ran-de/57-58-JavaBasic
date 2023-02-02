10e8456b2ff7e404057aa8aaae00f91a71175fe2
StanislavPB <perminovsb@gmail.com>
2023-02-02 21:13:52 +0200

    add: lesson 10 lessoncode

---
+++ src/lessons/lesson10/homework3/RandomExample.java
@@ -0,0 +1,12 @@
+package lessons.lesson10.homework3;
+
+import java.util.Random;
+
+public class RandomExample {
+    public static void main(String[] args) {
+
+
+        Random random = new Random();
+
+    }
+}
