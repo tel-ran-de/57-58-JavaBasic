36aa2240b1780da0e0c0b4023b41bf5087127a4e
StanislavPB <perminovsb@gmail.com>
2023-02-07 23:08:15 +0200

    add: lesson 11 lessoncode

--- src/main/java/lessons/lesson11/PersonDbDemo.java
+++ src/main/java/lessons/lesson11/PersonDbDemo.java
@@ -2,25 +2,32 @@ package lessons.lesson11;
 
 public class PersonDbDemo {
     public static void main(String[] args) {
-        PersonUtil pu = new PersonUtil();
 
-        pu.addNewPerson(pu.createPerson(1, "Petr",18));
-        pu.addNewPerson(pu.createPerson(2, "Alex",19));
-        pu.addNewPerson(pu.createPerson(3, "Ruslan",18));
-        pu.addNewPerson(pu.createPerson(4, "Viktor",20));
-        pu.addNewPerson(pu.createPerson(5, "Andrey",19));
-        pu.addNewPerson(pu.createPerson(6, "Roman",17));
-        pu.addNewPerson(pu.createPerson(7, "Olga",21));
-        pu.addNewPerson(pu.createPerson(8, "Paven",20));
-        pu.addNewPerson(pu.createPerson(9, "Timofey",19));
-        pu.addNewPerson(pu.createPerson(10, "Mikhail",19));
+        PersonUtil personUtilService = new PersonUtil();
 
-        pu.printDB(pu.databasePerson);
+        Person newPerson = personUtilService.createPerson(1, "Petr",18)
+
+        personUtilService.addNewPerson(newPerson);
+
+        newPerson = personUtilService.createPerson(2, "Alex",19);
+
+        personUtilService.addNewPerson(newPerson);
+
+        personUtilService.addNewPerson(personUtilService.createPerson(3, "Ruslan",18));
+        personUtilService.addNewPerson(personUtilService.createPerson(4, "Viktor",20));
+        personUtilService.addNewPerson(personUtilService.createPerson(5, "Andrey",19));
+        personUtilService.addNewPerson(personUtilService.createPerson(6, "Roman",17));
+        personUtilService.addNewPerson(personUtilService.createPerson(7, "Olga",21));
+        personUtilService.addNewPerson(personUtilService.createPerson(8, "Paven",20));
+        personUtilService.addNewPerson(personUtilService.createPerson(9, "Timofey",19));
+        personUtilService.addNewPerson(personUtilService.createPerson(10, "Mikhail",19));
+
+        personUtilService.printDB(personUtilService.databasePerson);
 
         System.out.println("---------");
-        Person[] arrayAgeControl = pu.chooseByAge(pu.databasePerson, 19);
+        Person[] arrayAgeControl = personUtilService.chooseByAge(personUtilService.databasePerson, 19);
 
-        pu.printDB(arrayAgeControl);
+        personUtilService.printDB(arrayAgeControl);
 
     }
 }
